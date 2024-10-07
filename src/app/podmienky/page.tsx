1a16bd1e51e392f32fe1aa6b96edaded5b43b308
janchi.source <janchi.dobak@gmail.com>
2024-10-07 12:46:44 +0200

    fixing podmienky/page.tsx

--- src/app/podmienky/page.tsx
+++ src/app/podmienky/page.tsx
@@ -1,25 +1,17 @@
 // src/app/layout.tsx
 
-import React from 'react';
-import { Inter } from 'next/font/google';
-import NavBar from '../../components/NavBar'; // Correct the path as necessary
-
-const inter = Inter({ subsets: ['latin'] });
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 export const metadata = {
   title: 'Zochook',
   description: 'A social media application similar to Instagram.',
 };
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function Podmienky() {
   return (
-    <html lang="en">
-      <body className={inter.className} style={{ paddingBottom: '56px' }}> {/* Adjust padding as necessary */}
-        <main>{children}</main>
-        <NavBar />
-      </body>
-    </html>
+    <Container>
+      <Typography> Stránka o mne </Typography>
+    </Container>
   );
-};
-
-export default Layout;
+}
