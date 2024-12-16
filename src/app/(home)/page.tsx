7f089e874f514f2ad89939b0a082fbbefb632b6d
janchi-source <janchi.dobak@gmail.com>
2024-12-16 12:04:37 +0100

    redirect to posts after login

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,12 +6,16 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/authOptions';
 import NonAuthPageView from '@/sections/NonAuthHomeView';
 import AuthPageView from '@/sections/AuthHomeView';
+import { redirect } from "next/navigation";
 
 
 export const metadata = { title: "Domov | Kolcobos" };
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/prispevok");
+  }
 
   return session ? <AuthPageView session={session} /> : <NonAuthPageView />;
 }
