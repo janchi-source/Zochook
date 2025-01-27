b47a157b9bb0f9d3b7c063e3bb5063a498df06f6
janchi-source <janchi.dobak@gmail.com>
2025-01-27 10:48:19 +0100

    ez fix numero sex

--- src/sections/SignInView.tsx
+++ src/sections/SignInView.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 import GoogleIcon from "@mui/icons-material/Google";
 import { useState } from "react";
 import Alert from "@mui/material/Alert";
-import SignInUpViewLink from "../components/CustomLink";
+import Link from "next/link";
 
 export default function SignInView() {
   const [error] = useState<string | null>(null);
@@ -41,10 +41,13 @@ export default function SignInView() {
       {error && <Alert severity="error">{error}</Alert>}
 
       <Typography variant="body2" sx={{ mt: 3 }}>
-        Don't have an account? <SignInUpViewLink href="/auth/registracia" text="Create one!" />
+        Don&apos;t have an account?{" "}
+        <Link href="/auth/registracia" passHref>
+          <Button variant="text" sx={{ textTransform: "none", p: 0 }}>
+            Create one!
+          </Button>
+        </Link>
       </Typography>
-
-
     </Container>
   );
 }
